36260ab7c0fb14099c9f83ff1bbaa2f0a449b181
RobertTLange <robertlange0@gmail.com>
2021-11-19 14:28:14 +0100

    Add codecov

--- mle_scheduler/ssh/__init__.py
+++ mle_scheduler/ssh/__init__.py
@@ -1,8 +1,10 @@
+from .ssh_manager import SSH_Manager
 from .job_manage_ssh import submit_ssh, monitor_ssh
 from .file_manage_ssh import send_dir_ssh, copy_dir_ssh, delete_dir_ssh
 
 
 __all__ = [
+    "SSH_Manager",
     "submit_ssh",
     "monitor_ssh",
     "send_dir_ssh",
